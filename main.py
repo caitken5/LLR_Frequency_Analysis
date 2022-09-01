a79a71ca3838eab50299aaa1330de125d7293af9
sobriqu83 <caitken5@uwo.ca>
2022-09-01 12:22:42 -0400

    Added data_header changes. Added new folder saving paths. Added variables to control what prints and what doesn't, as well as a variables that controls if the file will be saved or if I want to view it as the code runs. Added code that will print all of the desired graphs.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import gc
 
@@ -7,13 +8,48 @@ import header as h
 
 source_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/4_LLR_DATA_SEGMENTATION/" \
                 "NPZ_FILES_BY_TARGET"
-storage_name = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+f_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                "FFT_FORCE"
+v_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
                 "FFT_VELOCITY"
+df_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_dFORCE"
+dv_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_dVELOCITY"
+fa_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_FORCE-ANGLE"
+va_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_VELOCITY-ANGLE"
+dfa_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_dFORCE-ANGLE"
+dva_graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/6_LLR_FREQ_GRAPHS/" \
+                  "FFT_dVELOCITY-ANGLE"
+
+# A - dFxy_Mag | B - dVxy_Mag | C - Fxy_Angle_Smooth | D - Vxy_Angle_Smooth | E - dFxy_Angle_Smooth | F - dVxy_Angle_Smooth
+
+
+# Some variables to control what graphs save and which don't.
+f_graph = False
+v_graph = False
+df_graph = True
+dv_graph = True
+fa_graph = True
+va_graph = True
+dfa_graph = True
+dva_graph = True
+save_graphs = True
+
+
+# Define some constants.
 freq = 100
 rel_freq = 15
+rel_freq_2 = 5
 
 if __name__ == '__main__':
     print("Running frequency analysis...")
+    if save_graphs:
+        matplotlib.use('Agg')
+        print(" File_name, orig_cc_x, new_cc_x, orig_cc_y, new_cc_y")
     for file in os.listdir(source_folder):
         if file.endswith('.npz'):
             task_number = h.get_task_number(file)
@@ -26,21 +62,187 @@ if __name__ == '__main__':
                 data = np.load(source_file, allow_pickle=True)
                 # Unpack the data.
                 ragged_list, target_list, target_i = h.load_npz(data)
-                signal = np.asarray(target_list[:, h.data_header.index("Vxy_Mag")]).astype(float)
-                f, fft = h.freq_analysis(signal, freq)
-                # Find value for relevant frequency.
-                rel_freq_ind = f[(f < rel_freq)]
-                # Plotting of data here.
-                fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
-                fig.set_size_inches(25, 8)
-                ax1 = fig.add_subplot(111)
-                ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
-                # Set some labels.
-                file_name = file.split('.')[0]
-                ax1.set_xlabel("Frequency (Hz)")
-                ax1.set_ylabel("Amplitude (g/Hz)")
-                ax1.set_title("Absolute FFT of Velocity Magnitude Profile for Sample " + file_name)
-                save_str = storage_name + '/' + file_name
-                plt.savefig(fname=save_str)
-                fig.clf()
-                gc.collect()
+                if f_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("Fxy_Mag")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Force Magnitude Profile for Sample " + file_name)
+                    save_str = f_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if v_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("Vxy_Mag")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Velocity Magnitude Profile for Sample " + file_name)
+                    save_str = v_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if dfa_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("dFxy_Mag")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Force Derivative Profile for Sample " + file_name)
+                    save_str = df_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if dva_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("dVxy_Mag")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Velocity Derivative Profile for Sample " + file_name)
+                    save_str = dv_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if fa_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("Fxy_Angle_Smooth")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq_2)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Force Angle Profile for Sample " + file_name)
+                    save_str = fa_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if va_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("Vxy_Angle_Smooth")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq_2)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Velocity Angle Profile for Sample " + file_name)
+                    save_str = va_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if dfa_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("dFxy_Angle_Smooth")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Force Angle Derivative Profile for Sample " + file_name)
+                    save_str = dfa_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
+                if dva_graph:
+                    signal = np.asarray(target_list[:, h.data_header.index("dFxy_Angle_Smooth")]).astype(float)
+                    f, fft = h.freq_analysis(signal, freq)
+                    # Find value for relevant frequency.
+                    rel_freq_ind = f[(f < rel_freq)]
+                    # Plotting of data here.
+                    fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                    fig.set_size_inches(25, 8)
+                    ax1 = fig.add_subplot(111)
+                    ax1.plot(rel_freq_ind, fft[:rel_freq_ind.shape[0]], label="FFT for relevant frequencies")
+                    # Set some labels.
+                    file_name = file.split('.')[0]
+                    ax1.set_xlabel("Frequency (Hz)")
+                    ax1.set_ylabel("Amplitude (g/Hz)")
+                    ax1.set_title("Absolute FFT of Velocity Angle Derivative Profile for Sample " + file_name)
+                    save_str = dva_graph_folder + '/' + file_name
+                    if save_graphs:
+                        plt.savefig(fname=save_str)
+                        fig.clf()
+                        gc.collect()
+                    else:
+                        plt.show()
+                        plt.close()
